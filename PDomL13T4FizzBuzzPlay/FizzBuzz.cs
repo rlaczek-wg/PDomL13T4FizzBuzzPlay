0775383f48e8b8eaa5eceaba0561716a75205d9d
Rafal Laczek <rafal_laczek@yahoo.no>
2021-03-15 15:27:56 +0100

    Commit Program and FizzBuzz

---
+++ PDomL13T4FizzBuzzPlay/FizzBuzz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDomL13T4FizzBuzzPlay
+{
+   public class FizzBuzz
+    {
+        string result;
+
+
+        public string Result()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Podaj liczbe");
+                    int myNumber = int.Parse(Console.ReadLine());
+                    if (myNumber % 3 == 0)
+                    {
+                       result = "Fiss";
+                    }
+                    else if(myNumber % 5 == 0)
+                    {
+                        result = "Buzz";
+                    }
+                    else if (myNumber % 3 == 0 && myNumber % 5 == 0)
+                    {
+                        result = "FissBuzz";
+                    }
+                    else if (myNumber % 3 != 0 && myNumber % 5 != 0)
+                    {
+                        result = "Liczba "+ myNumber.ToString() +" jest niepodzielna ani przez 3 ani 5 ";
+                    } 
+                }
+                catch
+                {
+                    Console.WriteLine("Podana wartosc nie jest prawdopodobnie liczba :(");
+
+                }
+               return result;
+            }
+
+     
+
+        }
+    }
+}
---
+++ PDomL13T4FizzBuzzPlay/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PDomL13T4FizzBuzzPlay
+{
+    class Program
+    {
+        private static string result;
+        static void Main(string[] args)
+        {
+            string toStop;
+            while (true)
+            {
+                var fizzBuzz = new FizzBuzz();
+                result = fizzBuzz.Result();
+                Console.WriteLine(result);
+                
+                Console.WriteLine("Nacisnij Enter aby kontynuwac lub Y jeli chcesz ewentualnie przerwac gre.");
+                toStop = Console.ReadLine();
+                if (toStop == "y")
+                {
+                    Console.WriteLine("Nacisnales Y i gra zostaje przerwana");
+                    break;
+                }
+                
+            }
+
+        }
+    }
+}
